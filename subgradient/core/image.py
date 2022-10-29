fcff92e3d6d9e79b3b9575c4934639a46b18280d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 18:21:21 +0000

    Adding tarfile member sanitization to extractall()

--- subgradient/core/image.py
+++ subgradient/core/image.py
@@ -45,7 +45,26 @@ class ImageManage(LoggingConfigurable):
       if 'tar.gz' in os.path.normpath(building_folder):
         with tarfile.open(os.path.join(self.dockerfile_folder, building_folder), 'r:gz') as tar:
           random_folder, dockerfile_folder = os.path.normpath(building_folder).split('/')
-          tar.extractall(os.path.join(self.dockerfile_folder, random_folder))
+          def is_within_directory(directory, target):
+              
+              abs_directory = os.path.abspath(directory)
+              abs_target = os.path.abspath(target)
+          
+              prefix = os.path.commonprefix([abs_directory, abs_target])
+              
+              return prefix == abs_directory
+          
+          def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+          
+              for member in tar.getmembers():
+                  member_path = os.path.join(path, member.name)
+                  if not is_within_directory(path, member_path):
+                      raise Exception("Attempted Path Traversal in Tar File")
+          
+              tar.extractall(path, members, numeric_owner=numeric_owner) 
+              
+          
+          safe_extract(tar, os.path.join(self.dockerfile_folder,random_folder))
           building_folder = '%s/%s'%(random_folder, dockerfile_folder.replace(".tar.gz", ""))
 
       elif 'zip' in os.path.normpath(building_folder):
